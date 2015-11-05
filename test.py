f622746b11d735e38ee1c3568f9c1dd71efdbfa9
yacht220 <yizhou.zhou@outlook.com>
2015-11-05 11:41:23 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#class test():
+    #def test_fuc():
+def test_func(x):
+  if x==1:
+    print 'x is 1'
+    print 'Still in if block'
+  print 'outside if block'
+  return x
+
+test_func(2)
